3c3c92431d5fafcc4cd0ff821f603de0a207d38f
Saman <samansei@bu.edu>
2015-12-02 18:22:18 -0500

    corrected plots

--- bursting-drop/entire/mesh025/pgfplot.m
+++ bursting-drop/entire/mesh025/pgfplot.m
@@ -1,19 +1,21 @@
 
-R0 = 2;
+R0 = 2;  % droplet radius
+
+XY0 = [X0 (R0+Y0)/R0];
+save('gamma0.dat', 'XY0', '-ascii')
+
 XY1 = [X1 (R0+Y1)/R0];
 save('gamma1.dat', 'XY1', '-ascii')
 
 XY2 = [X2 (R0+Y2)/R0];
 save('gamma2.dat', 'XY2', '-ascii')
 
-XY3 = [X3 (R0+Y3)/R0];
-save('gamma3.dat', 'XY3', '-ascii')
+%XY3 = [X3 (R0+Y3)/R0];
+%save('gamma3.dat', 'XY3', '-ascii')
 
-XY4 = [X4 (R0+Y4)/R0];
-save('gamma4.dat', 'XY4', '-ascii')
+%XY4 = [X4 (R0+Y4)/R0];
+%save('gamma4.dat', 'XY4', '-ascii')
 
 XY5 = [X5 (R0+Y5)/R0];
 save('gamma5.dat', 'XY5', '-ascii')
 
-XY6 = [X6 (R0+Y6)/R0];
-save('gamma6.dat', 'XY6', '-ascii')
